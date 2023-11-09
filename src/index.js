c9b40ded624f0c01ea544417de99dff3a9a11d17
janviereric <janviereric@icloud.com>
2023-11-09 18:29:29 +0100

    Mise en place de l'architecture de base

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./style.css";
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /(node_modules)/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./src/img/*",
+          to: "./",
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "./src/index.html"),
+    }),
+  ],
+  stats: "minimal",
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    open: true,
+    static: path.resolve(__dirname, "./dist"),
+    historyApiFallback: {
+      index: "index.html",
+    },
+    watchFiles: ["./src/**"],
+    port: 4000,
+    hot: true,
+  },
+};
