b1252ab3a2a1c0dbc0f11520d2141dba80376be0
janviereric <janviereric@icloud.com>
2023-11-10 23:21:27 +0100

    Dessiner la carte

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import "./style.css";
 
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+const context2D = canvas.getContext("2d");
+
+const drawMap = () => {
+  context2D.fillStyle = "#3867d6";
+  context2D.fillRect(0, 0, 800, 800);
+};
+
+drawMap();
